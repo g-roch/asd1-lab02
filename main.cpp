dd01c2c8b0eca30c7cca9a9d43d8d5a78d269865
Gabriel Roch <gabriel.roch@heig-vd.ch>
2019-03-13 16:10:37 +0100

    main2 → main

--- main.cpp
+++ main.cpp
@@ -1,11 +1,17 @@
+/**
+ * @file main.cpp
+ * @brief LABO 2: PUZZLE IMPOSSIBLE
+ * 
+ * Résoudre le jeu puzzle impossible
+ *
+ * @author Gwendoline Dössegger
+ * @author Quentin Saucy
+ * @author Gabriel Roch
+ * @date 2019-03-06 au 2019-03-13
+ */
 
-
-#include <vector>
 #include <iostream>
 #include <algorithm>
-#include <cstdlib> // EXIT_SUCCESS
-//#include <iomanip> // setprecision
-//#include <cmath>
 
 #include "pieces.h"
 
@@ -13,243 +19,220 @@
 #define DROITE 1
 #define BAS 2
 #define GAUCHE 3
+#define NB_FACE 4
+#define NB_COL 3
 
 using namespace std;
 
-bool operator ==(const Piece& a, const Piece& b)
-{
-   return(a[0] == b[0]
-      and a[1] == b[1]
-      and a[2] == b[2]
-      and a[3] == b[3])
-      or (a[1] == b[0]
-      and a[2] == b[1]
-      and a[3] == b[2]
-      and a[0] == b[3])
-      or (a[2] == b[0]
-      and a[3] == b[1]
-      and a[0] == b[2]
-      and a[1] == b[3])
-      or (a[3] == b[0]
-      and a[0] == b[1]
-      and a[1] == b[2]
-      and a[2] == b[3]);
+/**
+ * @brief Afficher une piece (numéro + lettre)
+ * @example 4a
+ * @param os le flux de sortie
+ * @param rhs La piece à afficher
+ * @return le flux
+ */
+ostream& operator <<(ostream& os, const Piece& rhs);
+
+/**
+ * @brief Afficher une série de pièces
+ * @example 4a 1b 3d 7a …
+ * @param os le flux de sortie
+ * @param rhs Le vecteur de pieces à afficher
+ * @return le flux
+ */
+ostream& operator <<(ostream& os, const Pieces& rhs);
+
+/**
+ * @brief Compare deux pièces
+ * @param lhs Une pièce à comparer
+ * @param rhs Une deuxième pièce à comparer
+ * @retval true si c'est la même carte (à une rotation près)
+ * @retval false si ce n'est pas la même carte (à une rotation près)
+ */
+bool operator ==(const Piece& lhs, const Piece& rhs);
+
+/**
+ * @brief Tourne un pièce de 45°
+ * @param a La pièce à tourner
+ */
+void rotation(Piece& a);
+
+/**
+ * @brief Supprimer un carte d'un jeu de carte
+ * @param jeu Vecteur de pièce à modifier
+ * @param piece La pièce à supprimer du jeu
+ */
+void remove(Pieces& jeu, const Piece& piece);
+
+/**
+ * @brief Test si une piece peut rentrer dans la position suivante
+ * dans sur le plateau
+ * @param plateau Le plateau de jeu
+ * @param piece La pièce à insérer
+ * @note La piece est tournée pour tester la pièce, la rotation terminal est la
+ *    rotation permettant l'insertion, ou la rotation initiale.
+ * @retval true Si la pièce peux être insérée
+ */
+bool testPiece(const Pieces& plateau, Piece& piece);
+
+/**
+ * @brief Retourne l'image complémentaire de l'image fournie
+ * @param a L'image à inversé
+ * @return L'image complémentaire
+ */
+AttachementType complement(AttachementType a);
+
+/**
+ * @brief Résoud le jeux de manière récursive.
+ * @param plateau Le plateau de jeu actuelle
+ * @param deck Le deck comportant les pièces encore à poser
+ */
+void resolve(Pieces& plateau, Pieces deck);
+
+int main() {
+   Pieces jeux(PIECES);
+   Pieces plateau;
+
+   for(Piece piece: PIECES)
+   {
+      Pieces deck(PIECES);
+      remove(deck, piece);
+      for(int i = 0; i < NB_FACE; ++i)
+      {
+         plateau = {piece};
+         resolve(plateau, deck);
+         rotation(piece);
+      }
+   }
+   return EXIT_SUCCESS;
 }
-ostream& operator <<(ostream& os, Piece a)
+
+void resolve(Pieces& plateau, Pieces deck)
+{
+   if(deck.size() == 1) // s'il n'y a plus qu'une pièce
+   {
+      if(testPiece(plateau, deck.front()))
+      {
+         plateau.push_back(deck.front());
+         cout << plateau << endl;
+         plateau.pop_back();
+      }
+      return;
+   }
+
+   for(Piece piece: deck)
+   {
+      if(testPiece(plateau, piece)) 
+      { // si la pièce rentre sur le plateau, elle est tournée correctement par testPiece.
+
+         plateau.push_back(piece);
+
+         Pieces nouveauJeux(deck);
+         remove(nouveauJeux, piece);
+
+         resolve(plateau, nouveauJeux);
+
+         plateau.pop_back();
+      }
+   }
+}
+
+bool testPiece(const Pieces& plateau, Piece& piece)
+{
+   size_t position = plateau.size();
+   bool carteOK = false;
+   AttachementType rechercher;
+   if(position % NB_COL) // pas la premiere colonne
+   {
+      rechercher = complement(plateau[position-1][DROITE]);
+      for(int i = 0; i < NB_FACE; ++i)
+      {
+         carteOK = rechercher == piece[GAUCHE];
+         if(carteOK) break;
+         rotation(piece);
+      }
+      if(!carteOK) return false;
+   }
+   if(position >= NB_COL)
+   {
+      rechercher = complement(plateau[position-3][BAS]);
+      if(carteOK) carteOK = rechercher == piece[HAUT];
+      else
+      {
+         for(int i = 0; i < NB_FACE; ++i)
+         {
+            carteOK = rechercher == piece[HAUT];
+            if(carteOK) break;
+            rotation(piece);
+         }
+         if(!carteOK) return false;
+      }
+   }
+   return carteOK;
+
+}
+
+AttachementType complement(AttachementType a)
+{
+   if(a%2) return (AttachementType) ((int) a - 1);
+   else return (AttachementType) ((int) a + 1);
+}
+
+void remove(Pieces& jeux, const Piece& piece)
+{
+   jeux.erase(remove(jeux.begin(), jeux.end(), piece), jeux.cend());
+}
+
+
+void rotation(Piece& a)
+{
+   AttachementType tmp = a[0];
+   a[0] = a[3];
+   a[3] = a[2];
+   a[2] = a[1];
+   a[1] = tmp;
+}
+
+bool operator ==(const Piece& lhs, const Piece& rhs)
+{
+   return (lhs[0] == rhs[0] and lhs[1] == rhs[1] and lhs[2] == rhs[2] and lhs[3] == rhs[3])
+       or (lhs[1] == rhs[0] and lhs[2] == rhs[1] and lhs[3] == rhs[2] and lhs[0] == rhs[3])
+       or (lhs[2] == rhs[0] and lhs[3] == rhs[1] and lhs[0] == rhs[2] and lhs[1] == rhs[3])
+       or (lhs[3] == rhs[0] and lhs[0] == rhs[1] and lhs[1] == rhs[2] and lhs[2] == rhs[3]);
+}
+
+ostream& operator <<(ostream& os, const Pieces& rhs)
+{
+   bool frst = true;
+   for(const Piece& i : rhs)
+   {
+      if(frst) frst = false;
+      else os << ' ';
+      os << i;
+   }
+   return os ;
+}
+
+ostream& operator <<(ostream& os, const Piece& rhs)
 {
    for(size_t i = 0; i < PIECES.size(); ++i)
    {
-      if(PIECES[i] == a)
+      if(PIECES[i] == rhs)
       {
-         for(char j = 'a'; j < 'a' + 4; ++j)
-            if(a[j - 'a'] == PIECES[i][0])
-               return os << i+1 << j;
+         for(size_t j = 0; j < NB_FACE; ++j)
+         {
+            if(rhs[j] == PIECES[i][0])
+               switch(j)
+               {
+                  case 0: return os << i+1 << 'a';
+                  case 1: return os << i+1 << 'd';
+                  case 2: return os << i+1 << 'c';
+                  case 3: return os << i+1 << 'b';
+               }
+         }
       }
    }
    return os;
 }
 
-ostream& operator <<(ostream& os, Pieces a)
-{
-   for(const Piece& i : a)
-   {
-      os << i << ' ';
-   }
-   return os;
-}
 
-
-/**
- * Tourne une piece sur elle-même
- */
-void rotation(Piece& p)
-{
-   AttachementType tmp = p.at(0);
-   p.at(0) = p.at(3);
-   p.at(3) = p.at(2);
-   p.at(2) = p.at(1);
-   p.at(1) = tmp;
-}
-
-/**
- * Enleve toutes les pieces ne contenant pas l'image
- */
-void filtrerJeux(Pieces& jeux, AttachementType image)
-{
-   for(size_t i = jeux.size(); i > 0; --i)
-   {
-      if(jeux[i-1][0] != image 
-            and jeux[i-1][1] != image
-            and jeux[i-1][2] != image
-            and jeux[i-1][3] != image)
-      {
-         jeux.erase(jeux.cbegin() + i - 1);
-      }
-   }
-
-}
-
-/** 
- * Enleve toutes les pieces impossible à placer à une position
- */
-void filtrerJeux(Pieces& jeux, int position, const Pieces& plateau)
-{
-
-   vector<AttachementType> possibilite = {IMPOSSIBLE, IMPOSSIBLE, IMPOSSIBLE, IMPOSSIBLE};
-
-   switch(position) {
-      case 1:
-         possibilite[GAUCHE] = plateau.at(0).at(DROITE);
-         break;
-      case 2:
-         possibilite[GAUCHE] = plateau.at(1).at(DROITE);
-         break;
-      case 3:
-         possibilite[HAUT] = plateau.at(0).at(BAS);
-         break;
-      case 4:
-         possibilite[HAUT] = plateau.at(1).at(BAS);
-         possibilite[GAUCHE] = plateau.at(3).at(DROITE);
-         break;
-      case 5:
-         possibilite[HAUT] = plateau.at(2).at(BAS);
-         possibilite[GAUCHE] = plateau.at(4).at(DROITE);
-         break;
-      case 6:
-         possibilite[HAUT] = plateau.at(3).at(BAS);
-         break;
-      case 7:
-         possibilite[HAUT] = plateau.at(4).at(BAS);
-         possibilite[GAUCHE] = plateau.at(6).at(DROITE);
-         break;
-      case 8:
-         possibilite[HAUT] = plateau.at(5).at(BAS);
-         possibilite[GAUCHE] = plateau.at(7).at(DROITE);
-         break;
-   }
-
-   // deux fois la meme image → impossible
-   if(possibilite[HAUT] == possibilite[GAUCHE])
-   {
-      jeux = Pieces();
-      return;
-   }
-
-   for(AttachementType& i: possibilite)
-   {
-      if(i == IMPOSSIBLE) continue;
-
-      if(i%2) i = (AttachementType) ((int) i - 1);
-      else i = (AttachementType) ((int) i + 1);
-   }
-
-   // Enleve les carte n'aillant pas possibilite[HAUT]
-   if(possibilite[HAUT] != IMPOSSIBLE)
-      filtrerJeux(jeux, possibilite[HAUT]);
-
-   // Enleve les carte n'aillant pas possibilite[GAUCHE]
-   if(possibilite[GAUCHE] != IMPOSSIBLE)
-      filtrerJeux(jeux, possibilite[GAUCHE]);
-
-   for(size_t i = jeux.size(); i > 0; --i) // pour chaque piece
-   {
-
-      if(possibilite[GAUCHE] != IMPOSSIBLE and possibilite[HAUT] != IMPOSSIBLE)
-      {
-         bool carteOK = false;
-         for(int j = 0; j < 4; ++j)
-         {
-            if(possibilite[GAUCHE] == jeux[i-1][GAUCHE]
-               and possibilite[HAUT] == jeux[i-1][HAUT])
-            {
-               carteOK = true;
-               break;
-            }
-            rotation(jeux[i-1]);
-         }
-         if(!carteOK) jeux.erase(jeux.cbegin() + i - 1);
-      }
-      else if(possibilite[GAUCHE] != IMPOSSIBLE)
-      {
-         while(possibilite[GAUCHE] != jeux[i-1][GAUCHE])
-            rotation(jeux[i-1]);
-      }
-      else if(possibilite[HAUT] != IMPOSSIBLE)
-      {
-         while(possibilite[HAUT] != jeux[i-1][HAUT])
-            rotation(jeux[i-1]);
-      }
-      else
-      {
-         cout << "Error " << endl;
-      }
-
-      // pour chaque rotation
-      //    tester si ça joue
-   }
-
-
-
-   
-}
-
-void resoudreJeuxRec(Pieces jeux, int position, Pieces plateau) {
-
-   if(jeux.empty()) {
-      cout << "Réussi " << plateau << endl;
-   }
-
-   Pieces jeuxOrig(jeux);
-
-   filtrerJeux(jeux, position, plateau); // avec rotation
-
-   if(jeux.empty()) {
-      //cout << "echec @ " << position << " : " << jeuxOrig.size() << endl;
-      return;
-   }
-
-   plateau.push_back(jeux.front());
-
-   for(size_t i = 0; i < jeux.size(); ++i) {
-      plateau.at(position) = jeux[i];
-
-      /// TODO reprendre le jeux reçu et enlever la carte proposée
-      Pieces jeuxNouveau(jeuxOrig);
-      for(size_t j = 0; j < jeuxNouveau.size(); ++j) {
-         if(jeuxNouveau[j] == jeux[i])
-         {
-            jeuxNouveau.erase(jeuxNouveau.cbegin() + j);
-            break;
-         }
-      }
-      //jeuxNouveau.erase(jeuxNouveau.cbegin() + i);
-      resoudreJeuxRec(jeuxNouveau, position+1, plateau);
-   }
-
-
-
-  // cas triviaux
-  //     arrivée en pos = 9
-  //     un lien est faux
-}
-
-int main() {
-
-
-   for(size_t i = 0; i < PIECES.size(); ++i) {
-      Pieces jeux(PIECES);
-      Piece piece = PIECES.at(i);
-      jeux.erase(jeux.cbegin() + i);
-
-
-      for(size_t i = 0 ; i < 4; ++i) {
-         resoudreJeuxRec(jeux, 1, {piece});
-         rotation(piece);
-      }
-
-   }
-   return EXIT_SUCCESS;
-
-
-
-}
